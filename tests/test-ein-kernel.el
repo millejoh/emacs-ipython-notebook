ffc49e25885cfc12e08f6557696cb8bc7d3443c2
Takafumi Arakaki <aka.tkf@gmail.com>
2012-05-15 13:28:15 +0200

    Add test: ein:kernel-kill-check-url

--- tests/test-ein-kernel.el
+++ tests/test-ein-kernel.el
@@ -46,3 +46,23 @@
        (lambda ()))
       (ein:kernel-interrupt kernel)
       (should (equal got-url desired-url)))))
+
+(ert-deftest ein:kernel-kill-check-url ()
+  (let* ((port 8888)
+         (kernel (ein:kernel-new 8888))
+         (kernel-id "KERNEL-ID")
+         (desired-url "http://127.0.0.1:8888/kernels/KERNEL-ID")
+         got-url)
+    (flet ((url-retrieve (url &rest ignore) (setq got-url url))
+           (ein:kernel-start-channels (&rest ignore))
+           (ein:kernel-stop-channels (&rest ignore)))
+      (ein:kernel--handle-start-kernel
+       kernel
+       (list :kernel_id kernel-id)
+       (lambda ()))
+      (ein:kernel-kill kernel)
+      (let* ((l (split-string got-url "?"))
+             (got-url-0 (nth 0 l))
+             (got-url-1 (nth 1 l)))
+        (should (equal got-url-0 desired-url))
+        (should (string-match "^_=[0-9]+$" got-url-1))))))
