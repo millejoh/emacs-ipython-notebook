81c25bfc3d56d302ee1feeea77ecbd6a86254dd7
Takafumi Arakaki <aka.tkf@gmail.com>
2012-05-15 12:21:45 +0200

    Add test: ein:node-filter-is-and-not

--- tests/test-ein-node.el
+++ tests/test-ein-node.el
@@ -45,3 +45,29 @@
       (should (equal (mapcar #'ein:$node-data
                              (ein:node-filter en-list :not 'sag))
                      '("p" "e" "m" "g"))))))
+
+(ert-deftest ein:node-filter-is-and-not ()
+  (eintest:with-ewoc-mock
+    (let ((en-list (list (ein:node-new nil "s" '(spam sag))
+                         (ein:node-new nil "p" '(spam))
+                         (ein:node-new nil "e" '(egg))
+                         (ein:node-new nil "a" '(spam sag))
+                         (ein:node-new nil "g" '(egg sag))
+                         (ein:node-new nil "m" '(spam))
+                         (ein:node-new nil "g" '(egg)))))
+      (should (equal (mapcar #'ein:$node-data
+                             (ein:node-filter en-list :not 'spam :is 'sag))
+                     '("g")))
+      (should (equal (mapcar #'ein:$node-data
+                             (ein:node-filter en-list :is 'sag :not 'spam))
+                     '("g")))
+      (should (equal (mapcar #'ein:$node-data
+                             (ein:node-filter en-list :is 'spam :is 'sag))
+                     '("s" "a")))
+      (should (equal (mapcar #'ein:$node-data
+                             (ein:node-filter en-list :is 'sag :not 'spam
+                                              :not 'not-existing))
+                     '("g")))
+      (should (equal (mapcar #'ein:$node-data
+                             (ein:node-filter en-list :is 'sag :is 'spam))
+                     '("s" "a"))))))
