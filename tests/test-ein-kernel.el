5869348e43c3f33a328dff9484f016ed346796ef
Takafumi Arakaki <aka.tkf@gmail.com>
2012-05-15 13:22:22 +0200

    Add test: ein:kernel-interrupt-check-url

--- tests/test-ein-kernel.el
+++ tests/test-ein-kernel.el
@@ -29,3 +29,20 @@
        (lambda ()))
       (ein:kernel-restart kernel nil)
       (should (equal got-url desired-url)))))
+
+
+(ert-deftest ein:kernel-interrupt-check-url ()
+  (let* ((port 8888)
+         (kernel (ein:kernel-new 8888))
+         (kernel-id "KERNEL-ID")
+         (desired-url "http://127.0.0.1:8888/kernels/KERNEL-ID/interrupt")
+         got-url)
+    (flet ((url-retrieve (url &rest ignore) (setq got-url url))
+           (ein:kernel-start-channels (&rest ignore))
+           (ein:kernel-stop-channels (&rest ignore)))
+      (ein:kernel--handle-start-kernel
+       kernel
+       (list :kernel_id kernel-id)
+       (lambda ()))
+      (ein:kernel-interrupt kernel)
+      (should (equal got-url desired-url)))))
