b66832ca58d079e786bf2566b661d14e287ceada
Takafumi Arakaki <aka.tkf@gmail.com>
2012-05-22 13:56:24 +0200

    Encapsulate event handling in a class
    Various `ein:events-*' function was seeing buffer local variables
    which causes the problem when triggering events from non-notebook
    buffer.  To fix that problem, and prepare for the future change
    which may increase event channels, the status variable used for
    header line is encapsulated in a class.

--- ein-events.el
+++ ein-events.el
@@ -26,6 +26,7 @@
 ;;; Code:
 
 (eval-when-compile (require 'cl))
+(require 'eieio)
 
 (require 'ein-log)
 (require 'ein-utils)
@@ -54,32 +55,51 @@ See also `ein:ac-setup-maybe'."
        (setq header-line-format ein:header-line-format)))
 (add-hook 'after-change-major-mode-hook 'ein:header-line-setup-maybe)
 
-(ein:deflocal ein:events-status-notebook nil)
-(ein:deflocal ein:events-status-kernel nil)
+
+;;; Events handling class
+
+(ein:deflocal ein:@events nil
+  "Buffer local variable to hold an instance of `ein:events'.")
+
+(defclass ein:events ()
+  ((buffer :initarg :buffer :type buffer :document "Notebook buffer")
+   (status-notebook :initarg :status-notebook :initform nil :type symbol)
+   (status-kernel :initarg :status-kernel :initform nil :type symbol))
+  "Event handler class.")
+
+(defun ein:events-setup (buffer)
+  "Make a new event handler instance and setup local variable in the BUFFER.
+The newly created instance is returned by this function.  Event
+handler user must *not* use the buffer local variable directly.
+Use the variable returned by this function instead."
+  (with-current-buffer buffer
+    (setq ein:@events (ein:events "Events" :buffer buffer))
+    (setq header-line-format ein:header-line-format)
+    ein:@events))
 
 (defun ein:events-header-message-notebook ()
-  (case ein:events-status-notebook
+  (case (oref ein:@events :status-notebook)
     (notebook_saving.Notebook "Saving Notebook...")
     (notebook_saved.Notebook "Notebook is saved")
     (notebook_save_failed.Notebook "Failed to save Notebook!")))
 
 (defun ein:events-header-message-kernel ()
-  (case ein:events-status-kernel
+  (case (oref ein:@events :status-kernel)
     (status_idle.Kernel nil)
     (status_busy.Kernel "Kernel is busy...")
     (status_dead.Kernel "Kernel is dead. Need restart.")))
 
-(defun ein:events-trigger (event)
-  (ein:log 'debug "Event: %s" event)
-  (case event
+(defun ein:events-trigger (events event-type)
+  (ein:log 'debug "Event: %s" event-type)
+  (case event-type
     ((status_busy.Kernel status_idle.Kernel status_dead.Kernel)
-     (setq ein:events-status-kernel event))
+     (oset events :status-kernel event-type))
     ((notebook_saving.Notebook
       notebook_saved.Notebook
       notebook_save_failed.Notebook)
-     (setq ein:events-status-notebook event))
+     (oset events :status-notebook event-type))
     (t
-     (ein:log 'info "Unknown event: %s" event))))
+     (ein:log 'info "Unknown event: %s" event-type))))
 
 (provide 'ein-events)
 
