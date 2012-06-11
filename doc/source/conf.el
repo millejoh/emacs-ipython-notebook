a1b0603ded9f1c0f0cfb15cf448d304a49702c4b
Takafumi Arakaki <aka.tkf@gmail.com>
2012-06-11 04:38:46 +0200

    Fix: ein:mumamo-*-mode was not auto documented

--- doc/source/conf.el
+++ doc/source/conf.el
@@ -1,7 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/ein/")
 (add-to-list 'load-path "~/.emacs.d/el-get/websocket/")
+(add-to-list 'load-path "~/.emacs.d/el-get/nxhtml/util/") ; mumamo
 
 (require 'ein-notebooklist)
+(require 'ein-mumamo)
 (require 'ein-connect)
 
 ;; Load `wid-edit'.  Otherwise the following error will be raised:
