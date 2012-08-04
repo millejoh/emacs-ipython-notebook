e49811db5c0942a2748ff7e88d415d697ff7e3d8
Takafumi Arakaki <aka.tkf@gmail.com>
2012-08-04 04:01:27 +0200

    Add tests for filename translator

--- tests/test-ein-utils.el
+++ tests/test-ein-utils.el
@@ -85,3 +85,50 @@ def func():
 (ert-deftest ein:version ()
   "Check if `ein:version' can be parsed by `version-to-list'."
   (version-to-list ein:version))
+
+
+;;; File name translation
+
+(ert-deftest ein:filename-translations-from-to-tramp ()
+  (loop with ein:filename-translations =
+        `((8888 . ,(ein:tramp-create-filename-translator "HOST" "USER")))
+        with filename = "/file/name"
+        for port in '(7777 8888)    ; check for the one w/o translation
+        for emacs-filename = (ein:filename-from-python port filename)
+        do (message "emacs-filename = %s" emacs-filename)
+        do (should
+            (equal (ein:filename-to-python port emacs-filename)
+                   filename))))
+
+(ert-deftest ein:filename-translations-to-from-tramp ()
+  (loop with ein:filename-translations =
+        `((8888 . ,(ein:tramp-create-filename-translator "HOST" "USER")))
+        with filename = "/USER@HOST:/filename"
+        for port in '(8888)
+        do (should
+            (equal (ein:filename-from-python
+                    port (ein:filename-to-python port filename))
+                   filename))))
+
+(ert-deftest ein:filename-to-python-tramp ()
+  (let* ((port 8888)
+         (ein:filename-translations
+          `((,port . ,(ein:tramp-create-filename-translator "DUMMY")))))
+    (loop with python-filename = "/file/name"
+          for emacs-filename in '("/scpc:HOST:/file/name"
+                                  "/USER@HOST:/file/name")
+          do (should
+              (equal (ein:filename-to-python port emacs-filename)
+                     python-filename)))
+    ;; Error: Not a Tramp file name: /file/name
+    (should-error (ein:filename-to-python port "/file/name"))))
+
+(ert-deftest ein:filename-from-python-tramp ()
+  (loop with ein:filename-translations =
+        `((8888 . ,(ein:tramp-create-filename-translator "HOST" "USER")))
+        with python-filename = "/file/name"
+        for emacs-filename in '("/USER@HOST:/file/name" "/file/name")
+        for port in '(8888 7777)
+        do (should
+            (equal (ein:filename-from-python port python-filename)
+                    emacs-filename))))
