383f3003bf615f50a3009a8c391b37b891472c21
Takafumi Arakaki <aka.tkf@gmail.com>
2012-08-16 16:40:57 +0200

    Fix tests for console integration

--- tests/test-ein-notebook.el
+++ tests/test-ein-notebook.el
@@ -702,38 +702,6 @@ value of `ein:notebook-enable-undo'."
   (should-not (ein:notebook-test-notebook-name "a\\b"))
   (should (ein:notebook-test-notebook-name "This is a OK notebook name")))
 
-(ert-deftest ein:notebook-console-security-dir-string ()
-  (let ((ein:notebook-console-security-dir "/some/dir/")
-        (notebook (ein:notebook-new "DUMMY-URL-OR-PORT" "DUMMY-NOTEBOOK-ID")))
-    (should (equal (ein:notebook-console-security-dir-get notebook)
-                   ein:notebook-console-security-dir))))
-
-(ert-deftest ein:notebook-console-security-dir-list ()
-  (let ((ein:notebook-console-security-dir
-         '((8888 . "/dir/8888/")
-           ("htttp://dummy.org" . "/dir/http/")
-           (7777 . my-secret-directory)
-           (default . "/dir/default/")))
-        (my-secret-directory "/dir/secret/"))
-    (let ((notebook (ein:notebook-new 8888 "DUMMY-NOTEBOOK-ID")))
-      (should (equal (ein:notebook-console-security-dir-get notebook)
-                     "/dir/8888/")))
-    (let ((notebook (ein:notebook-new "htttp://dummy.org" "DUMMY-NOTEBOOK-ID")))
-      (should (equal (ein:notebook-console-security-dir-get notebook)
-                     "/dir/http/")))
-    (let ((notebook (ein:notebook-new 7777 "DUMMY-NOTEBOOK-ID")))
-      (should (equal (ein:notebook-console-security-dir-get notebook)
-                     "/dir/secret/")))
-    (let ((notebook (ein:notebook-new 9999 "DUMMY-NOTEBOOK-ID")))
-      (should (equal (ein:notebook-console-security-dir-get notebook)
-                     "/dir/default/")))))
-
-(ert-deftest ein:notebook-console-security-dir-func ()
-  (let ((ein:notebook-console-security-dir
-         '(lambda (x) (should (equal x "DUMMY-URL-OR-PORT")) "/dir/"))
-        (notebook (ein:notebook-new "DUMMY-URL-OR-PORT" "DUMMY-NOTEBOOK-ID")))
-    (should (equal (ein:notebook-console-security-dir-get notebook) "/dir/"))))
-
 (defun* eintest:notebook--check-nbformat (&optional orig_nbformat
                                                     orig_nbformat_minor
                                                     nbformat
