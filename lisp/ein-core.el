338ff5267f5c6d6e6a827b75fdcf89940dc00237
Takafumi Arakaki <aka.tkf@gmail.com>
2012-08-28 15:26:02 +0200

    Add ein-core.el

---
+++ lisp/ein-core.el
@@ -0,0 +1,34 @@
+;;; ein-core.el --- EIN core
+
+;; Copyright (C) 2012 Takafumi Arakaki
+
+;; Author: Takafumi Arakaki <aka.tkf at gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; ein-core.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; ein-core.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with ein-core.el.
+;; If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'ein)  ; get autoloaded functions into namespace
+(require 'ein-utils)
+
+(provide 'ein-core)
+
+;;; ein-core.el ends here
