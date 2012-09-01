15fa518ce0507081985e805b1706d1575a4a46c0
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-01 20:55:07 +0200

    Generate lisp/ein-loaddefs.el
    $ tools/update-autoloads.sh

---
+++ lisp/ein-loaddefs.el
@@ -0,0 +1,283 @@
+;;; ein-loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (ein:connect-to-default-notebook ein:connect-buffer-to-notebook
+;;;;;;  ein:connect-to-notebook-buffer ein:connect-to-notebook ein:connect-to-notebook-command)
+;;;;;;  "ein-connect" "ein-connect.el" (20546 22499))
+;;; Generated autoloads from ein-connect.el
+
+(autoload 'ein:connect-to-notebook-command "ein-connect" "\
+Connect to notebook.  When the prefix argument is given,
+you can choose any notebook on your server including the ones
+not yet opened.  Otherwise, already chose from already opened
+notebooks.
+
+\(fn &optional NOT-YET-OPENED)" t nil)
+
+(autoload 'ein:connect-to-notebook "ein-connect" "\
+Connect any buffer to notebook and its kernel.
+
+\(fn NBPATH &optional BUFFER NO-RECONNECTION)" t nil)
+
+(autoload 'ein:connect-to-notebook-buffer "ein-connect" "\
+Connect any buffer to opened notebook and its kernel.
+
+\(fn BUFFER-OR-NAME)" t nil)
+
+(autoload 'ein:connect-buffer-to-notebook "ein-connect" "\
+Connect BUFFER to NOTEBOOK.
+
+\(fn NOTEBOOK &optional BUFFER NO-RECONNECTION)" nil nil)
+
+(autoload 'ein:connect-to-default-notebook "ein-connect" "\
+Connect to the default notebook specified by
+`ein:connect-default-notebook'.  Set this to `python-mode-hook'
+to automatically connect any python-mode buffer to the
+notebook.
+
+\(fn)" nil nil)
+
+;;;***
+
+;;;### (autoloads (ein:console-open) "ein-console" "ein-console.el"
+;;;;;;  (20546 22396))
+;;; Generated autoloads from ein-console.el
+
+(autoload 'ein:console-open "ein-console" "\
+Open IPython console.
+To use this function, `ein:console-security-dir' and
+`ein:console-args' must be set properly.
+This function requires `Fabian Gallina's python.el`_ for now;
+It should be possible to support python-mode.el.  Patches are welcome!
+
+.. _`Fabian Gallina's python.el`: https://github.com/fgallina/python.el
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (ein:dev-stop-debug ein:dev-start-debug ein:dev-insert-mode-map)
+;;;;;;  "ein-dev" "ein-dev.el" (20546 22738))
+;;; Generated autoloads from ein-dev.el
+
+(autoload 'ein:dev-insert-mode-map "ein-dev" "\
+Insert mode-map into rst document.  For README.rst.
+
+\(fn MAP-STRING)" nil nil)
+
+(autoload 'ein:dev-start-debug "ein-dev" "\
+Enable EIN debugging support.
+When the prefix argument is given, debugging support for websocket
+callback (`websocket-callback-debug-on-error') is enabled.
+
+\(fn &optional WS-CALLBACK)" t nil)
+
+(autoload 'ein:dev-stop-debug "ein-dev" "\
+Disable debugging support enabled by `ein:dev-start-debug'.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (helm-ein-notebook-buffers anything-ein-notebook-buffers)
+;;;;;;  "ein-helm" "ein-helm.el" (20546 22709))
+;;; Generated autoloads from ein-helm.el
+
+(autoload 'anything-ein-notebook-buffers "ein-helm" "\
+Choose opened notebook using anything.el interface.
+
+\(fn)" t nil)
+
+(autoload 'helm-ein-notebook-buffers "ein-helm" "\
+Choose opened notebook using helm interface.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads (ein:iexec-mode) "ein-iexec" "ein-iexec.el" (20546
+;;;;;;  22664))
+;;; Generated autoloads from ein-iexec.el
+
+(autoload 'ein:iexec-mode "ein-iexec" "\
+Instant cell execution minor mode.
+Code cell at point will be automatically executed after any
+change in its input area.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads (ein:junk-rename ein:junk-new) "ein-junk" "ein-junk.el"
+;;;;;;  (20546 22682))
+;;; Generated autoloads from ein-junk.el
+
+(autoload 'ein:junk-new "ein-junk" "\
+Open a notebook to try random thing.
+Notebook name is determined based on
+`ein:junk-notebook-name-template'.
+
+When any prefix argument is given, this command asks notebook
+name to be used.  When two universal prefix arguments (i.e.,
+``C-u C-u``) are given, this command asks URL or port to use.
+
+\(fn NAME URL-OR-PORT)" t nil)
+
+(autoload 'ein:junk-rename "ein-junk" "\
+Rename the current notebook based on `ein:junk-notebook-name-template'
+and save it immediately.
+
+\(fn NAME)" t nil)
+
+;;;***
+
+;;;### (autoloads nil "ein-kernel" "ein-kernel.el" (20546 22357))
+;;; Generated autoloads from ein-kernel.el
+
+(defalias 'ein:kernel-url-or-port 'ein:$kernel-url-or-port)
+
+(defalias 'ein:kernel-id 'ein:$kernel-kernel-id)
+
+;;;***
+
+;;;### (autoloads nil "ein-notebook" "ein-notebook.el" (20546 22294))
+;;; Generated autoloads from ein-notebook.el
+
+(defalias 'ein:notebook-name 'ein:$notebook-notebook-name)
+
+;;;***
+
+;;;### (autoloads (ein:notebooklist-load ein:notebooklist-open-notebook-global
+;;;;;;  ein:notebooklist-list-notebooks ein:notebooklist-new-notebook-with-name
+;;;;;;  ein:notebooklist-new-notebook ein:notebooklist-reload ein:notebooklist-open)
+;;;;;;  "ein-notebooklist" "ein-notebooklist.el" (20546 22126))
+;;; Generated autoloads from ein-notebooklist.el
+
+(autoload 'ein:notebooklist-open "ein-notebooklist" "\
+Open notebook list buffer.
+
+\(fn &optional URL-OR-PORT NO-POPUP)" t nil)
+
+(autoload 'ein:notebooklist-reload "ein-notebooklist" "\
+Reload current Notebook list.
+
+\(fn)" t nil)
+
+(autoload 'ein:notebooklist-new-notebook "ein-notebooklist" "\
+Ask server to create a new notebook and open it in a new buffer.
+
+\(fn &optional URL-OR-PORT CALLBACK CBARGS)" t nil)
+
+(autoload 'ein:notebooklist-new-notebook-with-name "ein-notebooklist" "\
+Open new notebook and rename the notebook.
+
+\(fn NAME &optional URL-OR-PORT)" t nil)
+
+(autoload 'ein:notebooklist-list-notebooks "ein-notebooklist" "\
+Return a list of notebook path (NBPATH).  Each element NBPATH
+is a string of the format \"URL-OR-PORT/NOTEBOOK-NAME\".
+
+\(fn)" nil nil)
+
+(autoload 'ein:notebooklist-open-notebook-global "ein-notebooklist" "\
+Choose notebook from all opened notebook list and open it.
+Notebook is specified by a string NBPATH whose format is
+\"URL-OR-PORT/NOTEBOOK-NAME\".
+
+When used in lisp, CALLBACK and CBARGS are passed to `ein:notebook-open'.
+
+\(fn NBPATH &optional CALLBACK CBARGS)" t nil)
+
+(autoload 'ein:notebooklist-load "ein-notebooklist" "\
+Load notebook list but do not pop-up the notebook list buffer.
+
+For example, if you want to load notebook list when Emacs starts,
+add this in the Emacs initialization file::
+
+  (add-to-hook 'after-init-hook 'ein:notebooklist-load)
+
+or even this (if you want fast Emacs start-up)::
+
+  ;; load notebook list if Emacs is idle for 3 sec after start-up
+  (run-with-idle-timer 3 nil #'ein:notebooklist-load)
+
+You should setup `ein:url-or-port' or `ein:default-url-or-port'
+in order to make this code work.
+
+See also:
+`ein:connect-to-default-notebook', `ein:connect-default-notebook'.
+
+\(fn &optional URL-OR-PORT)" nil nil)
+
+;;;***
+
+;;;### (autoloads (ein:pseudo-console-mode) "ein-pseudo-console"
+;;;;;;  "ein-pseudo-console.el" (20546 22425))
+;;; Generated autoloads from ein-pseudo-console.el
+
+(autoload 'ein:pseudo-console-mode "ein-pseudo-console" "\
+Pseudo console mode.
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;;;### (autoloads (ein:shared-output-eval-string ein:shared-output-show-code-cell-at-point
+;;;;;;  ein:shared-output-pop-to-buffer) "ein-shared-output" "ein-shared-output.el"
+;;;;;;  (20546 22590))
+;;; Generated autoloads from ein-shared-output.el
+
+(autoload 'ein:shared-output-pop-to-buffer "ein-shared-output" "\
+Open shared output buffer.
+
+\(fn)" t nil)
+
+(autoload 'ein:shared-output-show-code-cell-at-point "ein-shared-output" "\
+Show code cell at point in shared-output buffer.
+It is useful when the output of the cell at point is truncated.
+See also `ein:cell-max-num-outputs'.
+
+\(fn)" t nil)
+
+(autoload 'ein:shared-output-eval-string "ein-shared-output" "\
+Evaluate a code.  Prompt will appear asking the code to run.
+This is handy when you want to execute something quickly without
+making a cell.  If the code outputs something, it will go to the
+shared output buffer.  You can open the buffer by the command
+`ein:shared-output-pop-to-buffer'.
+
+\(fn CODE &optional POPUP VERBOSE KERNEL)" t nil)
+
+;;;***
+
+;;;### (autoloads (ein:tb-show) "ein-traceback" "ein-traceback.el"
+;;;;;;  (20546 22642))
+;;; Generated autoloads from ein-traceback.el
+
+(autoload 'ein:tb-show "ein-traceback" "\
+Popup traceback viewer.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("debug-ein.el" "ein-ac.el" "ein-cell.el"
+;;;;;;  "ein-completer.el" "ein-core.el" "ein-events.el" "ein-kernelinfo.el"
+;;;;;;  "ein-kill-ring.el" "ein-log.el" "ein-mumamo.el" "ein-node.el"
+;;;;;;  "ein-notification.el" "ein-output-area.el" "ein-pager.el"
+;;;;;;  "ein-pkg.el" "ein-pytools.el" "ein-query.el" "ein-scratchsheet.el"
+;;;;;;  "ein-smartrep.el" "ein-subpackages.el" "ein-utils.el" "ein-websocket.el"
+;;;;;;  "ein-worksheet.el" "ein.el") (20546 22950 119162))
+
+;;;***
+
+(provide 'ein-loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; ein-loaddefs.el ends here
