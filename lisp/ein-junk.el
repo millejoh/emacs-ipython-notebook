a186e3de021fbf57995d8230fd36229c2d7b3c41
Takafumi Arakaki <aka.tkf@gmail.com>
2012-09-01 18:36:15 +0200

    ein:scratch-... -> ein:junk-notebook-name-template

--- lisp/ein-junk.el
+++ lisp/ein-junk.el
@@ -29,34 +29,37 @@
 (require 'ein-notebooklist)
 
 
-(defcustom ein:scratch-notebook-name-template "_scratch_%Y-%m-%d-%H%M%S_"
-  "Template of scratch notebook name.
+(define-obsolete-variable-alias 'ein:scratch-notebook-name-template
+  'ein:junk-notebook-name-template "0.2.0")
+
+(defcustom ein:junk-notebook-name-template "junk-%Y-%m-%d-%H%M%S"
+  "Junk notebook name template.
 This value is used from `ein:notebooklist-new-scratch-notebook'
 and `ein:notebook-rename-to-scratch-command'.  This must be a
 format string which can be passed to `format-time-string'."
   :type '(string :tag "Format string")
   :group 'ein)
 
-(defun ein:scratch-notebook-name ()
+(defun ein:junk-notebook-name ()
   "Generate new scratch notebook name based on `current-time' and
-`ein:scratch-notebook-name-template'."
-  (format-time-string ein:scratch-notebook-name-template (current-time)))
+`ein:junk-notebook-name-template'."
+  (format-time-string ein:junk-notebook-name-template (current-time)))
 
 (defun ein:notebooklist-new-scratch-notebook ()
   "Open a notebook to try random thing.
 Notebook name is determined based on
-`ein:scratch-notebook-name-template'."
+`ein:junk-notebook-name-template'."
   (interactive)
   (ein:notebooklist-new-notebook-with-name
-   (ein:scratch-notebook-name)
+   (ein:junk-notebook-name)
    (ein:default-url-or-port)))
 
 (defun ein:notebook-rename-to-scratch-command (name)
-  "Rename notebook based on `ein:scratch-notebook-name-template'
+  "Rename notebook based on `ein:junk-notebook-name-template'
 and save it immediately."
   (interactive
    (list (read-string "Rename notebook: "
-                      (ein:scratch-notebook-name))))
+                      (ein:junk-notebook-name))))
   (ein:notebook-rename-command name))
 
 (provide 'ein-junk)
