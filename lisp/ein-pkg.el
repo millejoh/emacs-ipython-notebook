70d9d0e9d9f56be9f28499d235fccbe32f4bb65a
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-09 11:56:45 +0200

    Make EIN depend on auto-complete
    see: #44

--- lisp/ein-pkg.el
+++ lisp/ein-pkg.el
@@ -1,4 +1,6 @@
 (define-package "ein"
   "0.2.0alpha0"
   "Emacs IPython Notebook"
-  '((websocket "0.9")))
+  '((websocket "0.9")
+    ;; `auto-complete' is not really a dependency, but who use EIN w/o AC?
+    (auto-complete "1.4")))
