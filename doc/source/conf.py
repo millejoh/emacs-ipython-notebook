9ad21d1455187fdc100e56e1da7e7127e19d488f
Takafumi Arakaki <aka.tkf@gmail.com>
2012-10-21 02:26:14 +0200

    Remove redundant option from conf.py
    It is in eldomain.py now.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -249,7 +249,6 @@ texinfo_documents = [
 
 # -- Options for EL domain -----------------------------------------------------
 
-emacs_executable = os.getenv("EMACS") or 'emacs'
 elisp_packages = {
     'ein': 'ein:',
     'helm': 'helm-',
