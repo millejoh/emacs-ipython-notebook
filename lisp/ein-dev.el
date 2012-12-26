6e352cd6d6aca6651d350764ba49a97d60d124a3
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-26 19:01:46 +0100

    Do not show sys-info for non-existing packages

--- lisp/ein-dev.el
+++ lisp/ein-dev.el
@@ -216,11 +216,12 @@ callback (`websocket-callback-debug-on-error') is enabled."
                                       (ein:eval-if-bound 'image-types))
    :ein (append (list :version (ein:version))
                 (ein:dev-dump-vars '("source-dir")))
-   :lib (mapcar #'ein:dev-sys-info--lib
-                '("websocket" "auto-complete" "mumamo"
-                  "auto-complete" "popup" "fuzzy" "pos-tip"
-                  "python" "python-mode" "markdown-mode"
-                  "smartrep" "anything" "helm"))))
+   :lib (ein:filter (lambda (info) (plist-get info :path))
+                    (mapcar #'ein:dev-sys-info--lib
+                            '("websocket" "auto-complete" "mumamo"
+                              "auto-complete" "popup" "fuzzy" "pos-tip"
+                              "python" "python-mode" "markdown-mode"
+                              "smartrep" "anything" "helm")))))
 
 (defun ein:dev-show-sys-info (&optional show-in-buffer)
   "Show Emacs and library information."
