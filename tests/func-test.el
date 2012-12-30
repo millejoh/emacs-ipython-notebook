3966a6c22f914cdcd97bcc78a108daf00f589867
Takafumi Arakaki <aka.tkf@gmail.com>
2012-12-30 15:59:16 +0100

    Fix ein:testing-delete-notebook-by-name
    ein:notebooklist-reload, which is called from success
    callback of ein:notebooklist-delete-notebook, needs to
    access ein:%notebooklist%.  However it was set to nil
    in ein:testing-delete-notebook-by-name just after the
    delete button is "clicked".  This was meant to detect
    if the delete process is finished, but was not working.
    To solve this problem, I add ein:notebooklist-after-open-hook
    and use it to detect end of the delete process.

--- tests/func-test.el
+++ tests/func-test.el
@@ -57,20 +57,29 @@ Make MAX-COUNT larger \(default 50) to wait longer before timeout."
           (ein:testing-get-notebook-by-name url-or-port "Untitled0")
         (ein:log 'debug "TESTING-GET-UNTITLED0-OR-CREATE end")))))
 
+(defvar ein:notebooklist-after-open-hook nil)
+
+(defadvice ein:notebooklist-url-retrieve-callback
+  (after ein:testing-notebooklist-url-retrieve-callback activate)
+  "Advice to add `ein:notebooklist-after-open-hook'."
+  (run-hooks 'ein:notebooklist-after-open-hook))
+
 (defun ein:testing-delete-notebook-by-name (url-or-port notebook-name)
   (ein:log 'debug "TESTING-DELETE-NOTEBOOK-BY-NAME start")
-  (with-current-buffer (ein:notebooklist-open url-or-port nil)
-    (ein:testing-wait-until (lambda () ein:%notebooklist%))
-    (save-excursion
-      (goto-char (point-min))
-      (search-forward notebook-name)
-      (move-beginning-of-line 1)
-      (search-forward "Delete")
-      (flet ((y-or-n-p (ignore) t))
-        (widget-button-press (point))))
-    (setq ein:%notebooklist% nil)
-    (ein:testing-wait-until (lambda () ein:%notebooklist%))
-    (ein:log 'debug "TESTING-DELETE-NOTEBOOK-BY-NAME end")))
+  (lexical-let (called-p)
+    (let ((ein:notebooklist-after-open-hook
+           (list (lambda () (setq called-p t)))))
+      (with-current-buffer (ein:notebooklist-open url-or-port nil)
+        (ein:testing-wait-until (lambda () ein:%notebooklist%))
+        (save-excursion
+          (goto-char (point-min))
+          (search-forward notebook-name)
+          (move-beginning-of-line 1)
+          (search-forward "Delete")
+          (flet ((y-or-n-p (ignore) t))
+            (widget-button-press (point))))
+        (ein:testing-wait-until (lambda () called-p))
+        (ein:log 'debug "TESTING-DELETE-NOTEBOOK-BY-NAME end")))))
 
 (ert-deftest ein:testing-get-untitled0-or-create ()
   (ein:log 'verbose "ERT TESTING-GET-UNTITLED0-OR-CREATE start")
