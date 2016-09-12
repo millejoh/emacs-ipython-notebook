f10d2896a297b02196d3c02e37e5f132fb5219de
John Miller <millejoh@mac.com>
2016-09-11 22:35:40 -0500

    Burn it up.

--- lib/request/doc/source/conf.el
+++
@@ -1,8 +0,0 @@
-(let* ((doc-source-path (file-name-directory load-file-name))
-       (project-path (concat doc-source-path "../..")))
-  (add-to-list 'load-path project-path))
-
-(require 'request)
-
-(provide 'deferred)     ; Pretend like deferred.el is already imported
-(require 'request-deferred)
