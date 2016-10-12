ea9933ec9d4df09013a1199f0018f1563f1f807e
John Miller <millejoh@mac.com>
2016-10-12 13:42:19 -0500

    Better return values, except for images
    Do a better job return results of code execution, however images are not
    being saved to file. Very unsure why this is.

--- lisp/ob-ein.el
+++ lisp/ob-ein.el
@@ -43,19 +43,40 @@
 
 (add-to-list 'org-src-lang-modes '("ein" . python))
 
-;; This function expands the body of a source code block by doing
-;; things like prepending argument definitions to the body, it should
-;; be called by the `org-babel-execute:template' function below.
-(defun org-babel-expand-body:ein (body params &optional processed-params)
-  "Expand BODY according to PARAMS, return the expanded body."
-  (require 'inf-template)
-  (let ((vars (nth 1 (or processed-params (org-babel-process-params params)))))
-    (concat
-     (mapconcat ;; define any variables
-      (lambda (pair)
-        (format "%s=%S"
-                (car pair) (org-babel-template-var-to-template (cdr pair))))
-      vars "\n") "\n" body "\n")))
+;;
+
+(defun ein:write-base64-image (img-string file)
+  (with-temp-file file
+    (let ((buffer-file-coding-system 'binary)
+          (require-final-newline nil))
+      (insert img-string)
+      (base64-decode-region (point-min) (point-max)))))
+
+(defun ein:return-mime-type (json file)
+  (loop
+   for key in (cond
+               ((functionp ein:output-type-preference)
+                (funcall ein:output-type-preference json))
+               (t ein:output-type-preference))
+   for type = (intern (format ":%s" key)) ; something like `:text'
+   for value = (plist-get json type)      ; FIXME: optimize
+   when (plist-member json type)
+   return
+   (case key
+     ((svg image/svg)
+      (progn
+        (ein:write-base64-decoded-image value file)
+        (format "[[%s]]" file)))
+     ((png image/png jpeg image/jpeg)
+      (progn
+        (ein:write-base64-image value file)
+        (format "[[%s]]" file)))
+     (t (plist-get json type)))))
+
+(defun org-babel-ein-process-outputs (outputs params)
+  (let ((file (cdr (assoc :file params))))
+    (loop for o in outputs
+          collecting (ein:return-mime-type o file))))
 
 ;; This is the main function which is called to evaluate a code
 ;; block.
@@ -74,10 +95,11 @@ emacs-ipython-notebook's facilities for communicating with
 jupyter kernels.
  This function is called by `org-babel-execute-src-block'"
   (let* ((processed-params (org-babel-process-params params))
-         (file (cdr (assoc :file params)))
+         (kernelspec (cdr (assoc :kernelspec params)))
          ;; set the session if the session variable is non-nil
          (session-kernel (org-babel-ein-initiate-session
-                          (cdr (assoc :session processed-params))))
+                          (cdr (assoc :session processed-params))
+                          kernelspec))
          ;; either OUTPUT or VALUE which should behave as described above
          (result-type (cdr (assoc :result-type processed-params)))
          ;; expand the body with `org-babel-expand-body:template'
@@ -86,8 +108,9 @@ jupyter kernels.
     (ein:shared-output-eval-string full-body nil nil session-kernel)
     (let ((cell (ein:shared-output-get-cell)))
       (ein:wait-until #'(lambda ()
-                          (not (null (slot-value cell 'outputs)))))
-      (slot-value cell 'outputs))))
+                          (not (null (slot-value cell 'outputs))))
+                      nil 10000)
+      (org-babel-ein-process-outputs (slot-value cell 'outputs) processed-params))))
 
 ;; This function should be used to assign any variables in params in
 ;; the context of the session environment.
@@ -108,25 +131,28 @@ jupyter kernels.
 (defun ein:org-babel-clean-url (url-or-port)
   (if (search ":" url-or-port)
       url-or-port
-    (string-to-int url-or-port)))
+    (string-to-number url-or-port)))
 
-(defun org-babel-ein-initiate-session (&optional session)
+(defun org-babel-ein-initiate-session (&optional session kernelspec)
   "If there is not a current inferior-process-buffer in SESSION then create.
  Return the initialized session."
-  (cond ((string= session "none")
+  (cond ((numberp session)
+         (ein:junk-new (ein:junk-notebook-name) kernelspec session))
+        ((string= session "none")
          (error "You must specify a notebook or kernelspec as the session variable for ein code blocks."))
         ((search "/" session)
          (let* ((url-or-port (ein:org-babel-clean-url (car (split-string session "/"))))
                 (path (ein:join-str "/" (rest (split-string session "/"))))
                 (nb
                  (or (ein:notebook-get-opened-notebook url-or-port path)
-                     (ein:notebook-open url-or-port path nil
+                     (ein:notebook-open url-or-port path kernelspec
                                         (lexical-let ((session session))
                                           (lambda ()
                                             (org-babel-ein-initiate-session session)))))))
            (ein:$notebook-kernel nb)))
         (t ;; Hope this is the name of a kernelspec we can start
-         (ein:junk-new (ein:junk-notebook-name) session))))
+         (let ((url-or-port (ein:org-babel-clean-url session)))
+           (ein:junk-new (ein:junk-notebook-name) kernelspec url-or-port)))))
 
 (provide 'ob-ein)
  ;;; ob-ein.el ends here
