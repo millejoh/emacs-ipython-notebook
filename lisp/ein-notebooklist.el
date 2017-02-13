f1d3fbe96713e85aaea2f1027c2cc1782e0e5a70
John Miller <John Miller>
2017-02-12 22:09:44 -0600

    More stable notebooklist updates.
    Adding a slight pause (sit-for, where have you been all my life?) after session
    queries seems to make notebooklist buffer updates much more stable.

--- lisp/ein-notebooklist.el
+++ lisp/ein-notebooklist.el
@@ -558,6 +558,7 @@ Notebook list data is passed via the buffer local variable
   (let ((api-version (ein:$notebooklist-api-version ein:%notebooklist%))
         (sessions (make-hash-table :test 'equal)))
     (ein:content-query-sessions sessions (ein:$notebooklist-url-or-port ein:%notebooklist%) t)
+    (sit-for 0.2) ;; FIXME: What is the optimum number here?
     (loop for note in (ein:$notebooklist-data ein:%notebooklist%)
           for urlport = (ein:$notebooklist-url-or-port ein:%notebooklist%)
           for name = (plist-get note :name)
