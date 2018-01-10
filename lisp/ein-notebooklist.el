2f722a31a22e7aabbd5b12a4fc0c30bfb374997e
John M. Miller <millejoh@mac.com>
2018-01-10 09:36:27 -0600

    ein-notebooklist: Fix for #260.
    Force a query of available kernelspecs when calling `ein:notebooklist-open'.
    Idea is to catch when user installs a new kernelspecs while ein is active.

--- lisp/ein-notebooklist.el
+++ lisp/ein-notebooklist.el
@@ -166,6 +166,7 @@ To suppress popup, you can pass a function `ein:do-nothing' as CALLBACK."
            (lambda (content)
              (pop-to-buffer
               (funcall #'ein:notebooklist-url-retrieve-callback content))))))
+    (ein:query-kernelspecs url-or-port)
     (ein:content-query-contents path url-or-port t success))
   ;(ein:notebooklist-get-buffer url-or-port)
   )
