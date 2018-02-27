c8fadccbc883f1e822daf55fc8ac442177aaa217
John Miller <millejoh@mac.com>
2018-02-27 14:39:08 -0600

    Spring cleaning the testing directory.

--- tests/notebook/nbformat4/mod.py
+++
@@ -1,6 +0,0 @@
-
-def f(x):
-    return 1.0/(x-1)
-
-def g(y):
-    return f(y+1)
--- tests/test-ein-smartrep.el
+++
@@ -1,8 +0,0 @@
-(eval-when-compile (require 'cl))
-(require 'ert)
-
-(require 'ein-smartrep)
-
-(ert-deftest ein:smartrep-notebook-mode-alist-fboundp ()
-  (loop for (k . f) in ein:smartrep-notebook-mode-alist
-        do (should (fboundp f))))
