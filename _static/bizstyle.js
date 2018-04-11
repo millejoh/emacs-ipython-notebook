8f2e449e59c5ea298f682ec768a040b833e10a20
John Miller <millejoh@mac.com>
2018-04-11 12:08:54 -0500

    Update

--- _static/bizstyle.js
+++ _static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("Emacs IPython Notebook 0.13.0 documentation");
+        $("li.nav-item-0 a").text("Emacs IPython Notebook 0.13.1 documentation");
     }
 });
