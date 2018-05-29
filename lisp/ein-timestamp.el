359d4caabc82c3ec27f7e17738b9c9750a1e7a70
Sam Steingold <sds@gnu.org>
2018-05-29 17:20:54 -0400

    avoid compilation warnings about unused lexicals and fix minor typos

--- lisp/ein-timestamp.el
+++ lisp/ein-timestamp.el
@@ -17,7 +17,7 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with ein-kernel.el.  If not, see <http://www.gnu.org/licenses/>.
+;; along with ein-timestamp.el.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
