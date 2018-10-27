e1b26550b21fd2c5bc62b42b021d55de09994806
John Miller <millejoh@mac.com>
2018-10-26 21:20:44 -0500

    ein-dev: Don't try to require ein-autoloads.el
    Fix is mostly so we can continue to build the documentation.

--- lisp/ein-dev.el
+++ lisp/ein-dev.el
@@ -74,6 +74,7 @@
 (defun* ein:dev-require-all (&key (ignore-p #'ignore))
   (loop for f in (directory-files ein:source-dir nil "^ein-.*\\.el$")
         unless (or (equal f "ein-pkg.el")
+                   (equal f "ein-autoloads.el")
                    (funcall ignore-p f))
         do (require (intern (file-name-sans-extension f)) nil t))
   ;; For `widget-button-press':
