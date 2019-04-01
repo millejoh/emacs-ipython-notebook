e178f92772f73063c514af02653bacd6ad44fe58
finalpatch <fengli@gmail.com>
2019-04-01 21:13:18 +1100

    fix last commit

--- lisp/ob-ein.el
+++ lisp/ob-ein.el
@@ -102,8 +102,9 @@
       (let ((el (org-element-context))
             (id (org-id-new 'none)))
         (goto-char (org-element-property :begin el))
-        (indent-according-to-mode)
-        (insert (format "#+NAME: %s\n" id))
+        (back-to-indentation)
+        (split-line)
+        (insert (format "#+NAME: %s" id))
         id))))
 
 (defun ein:org-register-lang-mode (lang-name lang-mode)
