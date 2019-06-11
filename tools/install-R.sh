fbd01d1ca1671b1476ffae05029fefe179c96739
dickmao <none>
2019-06-10 21:21:53 -0400

    more instances of the same

--- tools/install-R.sh
+++ tools/install-R.sh
@@ -17,8 +17,7 @@ if [ "x$TRAVIS_OS_NAME" = "xlinux" ] ; then
     R -e "install.packages('IRkernel', repos='http://cran.mirrors.hoobly.com')"
     R -e "IRkernel::installspec()"
 elif [ "x$TRAVIS_OS_NAME" = "xosx" ]; then
-    brew update
-    brew list r &>/dev/null || brew install r
+    brew list r &>/dev/null || HOMEBREW_NO_AUTO_UPDATE=1 brew install r
     R -e "install.packages('IRkernel', repos='http://cran.mirrors.hoobly.com')"
     R -e "IRkernel::installspec()"
 fi
--- tools/install-virtualenv.sh
+++ tools/install-virtualenv.sh
@@ -9,7 +9,7 @@ WORKDIR=${HOME}/local
 . tools/retry.sh
 
 if [ "x$TRAVIS_OS_NAME" = "xosx" ]; then
-    brew list pyenv-virtualenv || HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv-virtualenv
+    brew list pyenv-virtualenv &>/dev/null || HOMEBREW_NO_AUTO_UPDATE=1 brew install pyenv-virtualenv
 
     case "${TOXENV}" in
         py27)
