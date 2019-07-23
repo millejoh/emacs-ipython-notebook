d4c76b5915363178bb36203d69349d216e81de56
Miller <john.miller3@honeywell.com>
2019-07-23 06:55:15 -0600

    Formatting and cleanup.

--- tasks.py
+++ tasks.py
@@ -8,9 +8,11 @@ from invoke import task
 def autoloads(c):
     c.run("""emacs -Q --batch --eval "(package-initialize)" --eval "(package-generate-autoloads \\"ein\\" \\"./lisp\\")" """)
 
+
 @task
 def clean_elc(c):
     c.run("""cask clean-elc""")
+
 
 @task
 def clean(c):
@@ -22,20 +24,25 @@ def clean(c):
     [f.unlink() for f in p.glob('features/Untitled*.ipynb')]
     [f.unlink() for f in p.glob('test/Untitled*.ipynb')]
 
+
 @task
 def dist_clean(c):
     clean(c)
+    p = Path('.')
     if Path('dist/').exists():
         [f.unlink() for f in p.glob('dist/*')]
         Path('dist').rmdir()
+
 
 @task
 def test_compile(c):
     c.run("cask install")
 
+
 @task
 def test_jupyterhub(c):
     c.run("cask exec ecukes --tags @jupyterhub --reporter magnars")
+
 
 @task
 def test_poly(c):
@@ -44,10 +51,12 @@ def test_poly(c):
     c.run("cask exec ecukes --report magnars")
     Path('features/support/test-poly.el').unlink()
 
+
 @task
 def test_int(c):
     c.run("cask exec ert-runner -L ./lisp -L ./test -l test/testfunc.el test/test-func.el")
     c.run("cask exec ecukes --reporter magnars")
+
 
 @task
 def test_unit(c):
