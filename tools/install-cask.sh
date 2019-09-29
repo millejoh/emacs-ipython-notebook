ed4522b7876d07be8a45846acf6bb1f3c5cdac10
dickmao <none>
2019-09-29 12:40:20 -0400

    ob-lang cannot require org
    Generally, ob-lang modules cannot require org, else it gets into
    infinite require loop.
    
    This begs the question whether ob-ein is really used by anyone.

--- tools/install-cask.sh
+++ tools/install-cask.sh
@@ -11,19 +11,27 @@ CASKDIR=$WORKDIR/cask
 
 . tools/retry.sh
 
+update_elpa_keys() {
+    mkdir -p $HOME/.emacs.d/elpa/gnupg || true
+    chmod 700 $HOME/.emacs.d/elpa/gnupg
+    GPG=gpg
+    if which gpg2 ; then GPG=gpg2 ; fi
+    travis_retry ${GPG} --keyserver hkp://pool.sks-keyservers.net:80 --homedir $HOME/.emacs.d/elpa/gnupg --recv-keys 066DAFCB81E42C40
+    mkdir -p $(cask package-directory) || true
+    mkdir -p $HOME/.cask || true
+    rsync -azSHe ssh $HOME/.cask $(dirname $(dirname $(dirname $(cask package-directory))))
+    rsync -azSHe ssh $HOME/.emacs.d/elpa/gnupg $(cask package-directory)
+}
+
 cask_upgrade_cask_or_reset() {
     cask upgrade-cask || { rm -rf $HOME/.emacs.d/.cask && false; }
 }
 
 cask_install_or_reset() {
-    rsync -azSHe ssh $HOME/.cask $(dirname $(dirname $(dirname $(cask package-directory))))
-    chmod 700 $HOME/.emacs.d/elpa
-    gpg2 --keyserver hkp://pool.sks-keyservers.net:80 --homedir $HOME/.emacs.d/elpa --recv-keys 066DAFCB81E42C40
-    mkdir -p $(cask package-directory) || true
-    cp -R $HOME/.emacs.d/elpa/gnupg $(cask package-directory)
     cask install </dev/null
     find $(cask package-directory)/archives -print | xargs ls -l
     find $(cask package-directory)/gnupg -print | xargs ls -l
+
     # travis cache
     rsync -azSHe ssh $(dirname $(dirname $(cask package-directory))) $HOME/
 }
@@ -36,5 +44,6 @@ fi
 # Install dependencies for cider as descriped in ./Cask
 # Effect is identical to "make elpa", but here we can retry
 # in the event of network failures.
+update_elpa_keys
 travis_retry cask_upgrade_cask_or_reset
 travis_retry cask_install_or_reset && touch elpa-emacs
