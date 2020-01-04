6a525cde7f7efb200f61abc68360defe8184839c
dickmao <none>
2020-01-04 11:34:55 -0500

    bump version

--- lisp/ein-pkg.el
+++ lisp/ein-pkg.el
@@ -1,5 +1,5 @@
 (define-package "ein"
-  "0.16.2"
+  "0.17.0"
   "Emacs IPython Notebook"
   '((emacs "25")
     (websocket "20190620.338")
@@ -8,5 +8,6 @@
     (deferred "0.5")
     (polymode "20190426.1729")
     (markdown-mode "20171116.756")
+    (kubernetes "20200104.353")
     (dash "2.13.0")
     (s "1.11.0")))
