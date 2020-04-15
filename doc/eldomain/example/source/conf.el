0c52370100d9b22b000010f67e89ea31aa1b497b
dickmao <none>
2020-04-14 22:11:16 -0400

    Removing obsolete doc directory
    This directory contains only woe and
    misdirection (https://github.com/dickmao/emacs-ipython-notebook/issues/78#issuecomment-613773343).

--- doc/eldomain/example/source/conf.el
+++
@@ -1,2 +0,0 @@
-(add-to-list 'load-path (file-name-directory load-file-name))
-(require 'eldomain-example)
--- doc/update-gh-pages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd build/html/
-git add .
-if [ -n "$(git ls-files --deleted)" ]
-then
-    git ls-files --deleted | xargs git rm
-fi
-git commit -m "Update"
