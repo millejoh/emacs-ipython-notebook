1218426397f2faf8042f7e367b70f87bffcc3189
dickmao <none>
2020-11-17 07:56:50 -0500

    pick up more recent ipynb from run-remote

--- lisp/ein-ipynb-mode.el
+++ lisp/ein-ipynb-mode.el
@@ -35,7 +35,27 @@
 
 \\{ein:ipynb-mode-map}
 "
-  :group 'ein)
+  :group 'ein
+  :after-hook
+  (let* ((filename (file-name-nondirectory buffer-file-truename))
+         (remote-filename (concat (file-name-as-directory "run-remote") filename)))
+    (when (and (file-readable-p remote-filename)
+               (file-newer-than-file-p remote-filename filename)
+               (prog1
+                   (y-or-n-p "Corresponding run-remote is newer. Replace? (will first backup) ")
+                 (message "")))
+      (if-let ((make-backup-files t)
+               (where-to (funcall make-backup-file-name-function buffer-file-name)))
+          (let* (backup-inhibited
+                 (orig-hooks find-file-hook)
+                 (reassure (lambda ()
+                             (message "Backed up to %s" where-to)
+                             (setq find-file-hook orig-hooks))))
+            (backup-buffer)
+            (copy-file remote-filename filename t t)
+            (add-hook 'find-file-hook reassure nil)
+            (find-file-noselect filename))
+        (message "Backup failed.  Not replaced")))))
 
 (let ((map ein:ipynb-mode-map))
   (set-keymap-parent map js-mode-map)
