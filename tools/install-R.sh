917f2a0b6ca76ed9e966985ca6f19d8b57690d40
dickmao <none>
2021-01-02 09:02:37 -0500

    reverse errant hunk reverses

--- tools/install-R.sh
+++ tools/install-R.sh
@@ -2,16 +2,16 @@
 
 # install R for Actions CI
 
-set -x
+set -ex
 
 WORKDIR=${HOME}/local
 UNAME=$(uname -s)
 cd $WORKDIR
 if [ "x$UNAME" = "xLinux" ] ; then
     if [ ! -d ${WORKDIR}/R ]; then
-        wget http://cran.mirrors.hoobly.com/src/base/R-3/R-3.4.1.tar.gz
-        tar xvf R-3.4.1.tar.gz
-        cd R-3.4.1
+        wget http://cran.mirrors.hoobly.com/src/base/R-3/R-3.6.3.tar.gz
+        tar xvf R-3.6.3.tar.gz
+        cd R-3.6.3
         ./configure --prefix=${WORKDIR}/R
         make && make install
         find ${WORKDIR}/R -name R -print
@@ -21,6 +21,6 @@ elif [ "x$UNAME" = "xDarwin" ]; then
 fi
 mkdir -p ${WORKDIR}/R
 echo ".libPaths( c( '${WORKDIR}/R' , .libPaths() ) )" > ${HOME}/.Rprofile
-R -e "install.packages('IRkernel', repos='http://cran.mirrors.hoobly.com', lib='${WORKDIR}/R')"
+R -e "install.packages(c('pbdZMQ', 'repr', 'IRdisplay', 'IRkernel'), type='source', repos='http://cran.mirrors.hoobly.com', lib='${WORKDIR}/R')"
 R -e "IRkernel::installspec()"
 R --version
