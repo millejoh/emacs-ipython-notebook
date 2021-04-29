millejoh/emacs-ipython-notebook

commit 02ab452227197b42b18fd9fa0ae006b1a893f5a9

native json parsing

lisp/ein-kernel.el
@@ -176,20 +176,14 @@ CALLBACK of arity 1, the kernel.
       (ein:query-singleton-ajax
        (ein:url (ein:$kernel-url-or-port kernel) "api/sessions")
        :type "POST"
-       :data (json-encode
-              (cond ((<= (ein:$kernel-api-version kernel) 4)
-                     `(("notebook" .
-                        (("path" . ,path)))
-                       ,@(if kernelspec
-                             `(("kernel" .
-                                (("name" . ,(ein:$kernelspec-name kernelspec))))))))
-                    (t `(("path" . ,path)
-                         ("type" . "notebook")
-                         ,@(if kernelspec
-                               `(("kernel" .
-                                  (("name" . ,(ein:$kernelspec-name kernelspec))
-                                   ,@(if kernel-id
-                                         `(("id" . ,kernel-id)))))))))))
+       :data (ein:json-encode
+              `((path . ,path)
+                (type . "notebook")
+                ,@(if kernelspec
+                      `((kernel .
+                                ((name . ,(ein:$kernelspec-name kernelspec))
+                                 ,@(if kernel-id
+                                       `((id . ,kernel-id)))))))))
        :parser #'ein:json-read
        :complete (apply-partially #'ein:kernel-retrieve-session--complete kernel callback)
        :success (apply-partially #'ein:kernel-retrieve-session--success kernel callback)

Makefile
@@ -46,8 +46,9 @@ clean:
 	$(CASK) clean-elc
 	rm -rf test/test-install
 	rm -rf log
-	rm -f features/Untitled*.ipynb
+	rm -f features/[Uu]ntitled*
 	rm -f features/Renamed.ipynb
+	rm -f ert-profile*
 	rm -rf features/test-repo
 
 .PHONY: cask

features/support/env.el
@@ -100,7 +100,7 @@
              (alist-get
               'kernelspecs
               (let ((json-object-type 'alist))
-                (json-read-from-string
+                (json-read-from-string ;; intentionally not ein:json-read-from-string
                  (shell-command-to-string
                   (format "%s kernelspec list --json"
                           ein:jupyter-server-command)))))

lisp/ein-cell.el
@@ -260,15 +260,15 @@ WARNING: OBJ and SLOT are evaluated multiple times,
   (let ((cell (ein:cell-init (apply #'ein:cell-from-type
                                     (ein:cell--determine-cell-type data) args)
                              data)))
-    (aif (plist-get data :metadata)
+    (awhen (plist-get data :metadata)
       (ein:oset-if-empty cell 'metadata it))
     cell))
 
 (cl-defmethod ein:cell-init ((cell ein:codecell) data)
   (ein:oset-if-empty cell 'outputs (mapcar (lambda (o)
                                              (if (and (plist-member o :metadata)
-                                                        (not (plist-get o :metadata)))
-                                               (plist-put o :metadata (make-hash-table))
+                                                      (not (plist-get o :metadata)))
+                                                 (plist-put o :metadata (make-hash-table))
                                                o))
                                            (plist-get data :outputs)))
   (ein:oset-if-empty cell 'input (or (plist-get data :input)
@@ -783,7 +783,7 @@ If END is non-`nil', return the location of next element."
   from the kernel, one from the \"shell\" channel, and one from the \"iopub\"
   channel.  As a workaround, we remember the cell's traceback and ignore
   traceback outputs that are identical to the one we already have."
-  (let ((new-tb (plist-get json :traceback))
+  (let ((new-tb (append (plist-get json :traceback) nil))
         (old-tb (slot-value cell 'traceback)))
     (when (or
            (null old-tb)
@@ -826,11 +826,11 @@ Called from ewoc pretty printer via `ein:cell-insert-output'."
   (mapc (lambda (tb)
           (ein:cell-append-text tb)
           (ein:cell-append-text "\n"))
-        (let ((tb (plist-get json :traceback))
+        (let ((tb (append (plist-get json :traceback) nil))
               (level ein:cell-traceback-level))
           (if (and level (> (- (length tb) 2) level))
               (cons (substitute-command-keys
-                     "\nTruncated Traceback (Use \\[ein:tb-show-km] to view full TB):")
+                     "\nTruncated Traceback (Use \\<ein:notebook-mode-map>\\[ein:tb-show-km] to view full TB):")
                     (last tb (1+ level)))
             tb)))
   (ein:insert-read-only "\n"))

lisp/ein-contents-api.el
@@ -101,19 +101,8 @@ ERRBACK of arity 1 for the contents."
 (cl-defun ein:content-query-contents--success (url-or-port path callback
                                                &key data _symbol-status _response
                                                &allow-other-keys)
-  (let (content)
-    (setq content (ein:new-content url-or-port path data))
-    (when callback
-      (funcall callback content))))
-
-(defun ein:fix-legacy-content-data (data)
-  (if (listp (car data))
-      (cl-loop for item in data
-            collecting
-            (ein:fix-legacy-content-data item))
-    (if (string= (plist-get data :path) "")
-        (plist-put data :path (plist-get data :name))
-      (plist-put data :path (format "%s/%s" (plist-get data :path) (plist-get data :name))))))
+  (when callback
+    (funcall callback (ein:new-content url-or-port path data))))
 
 (defun ein:content-to-json (content)
   (let ((path (if (>= (ein:$content-notebook-version content) 3)
@@ -122,11 +111,12 @@ ERRBACK of arity 1 for the contents."
                            0
                            (or (cl-position ?/ (ein:$content-path content) :from-end t)
                                0)))))
-    (ignore-errors (json-encode `((:type . ,(ein:$content-type content))
-                                  (:name . ,(ein:$content-name content))
-                                  (:path . ,path)
-                                  (:format . ,(or (ein:$content-format content) "json"))
-                                  (:content ,@(ein:$content-raw-content content)))))))
+    (ignore-errors
+      (ein:json-encode `((type . ,(ein:$content-type content))
+                         (name . ,(ein:$content-name content))
+                         (path . ,path)
+                         (format . ,(or (ein:$content-format content) "json"))
+                         (content ,@(ein:$content-raw-content content)))))))
 
 (defun ein:content-from-notebook (nb)
   (let ((nb-content (ein:notebook-to-json nb)))
@@ -135,7 +125,7 @@ ERRBACK of arity 1 for the contents."
                        :url-or-port (ein:$notebook-url-or-port nb)
                        :type "notebook"
                        :notebook-version (ein:$notebook-api-version nb)
-                       :raw-content nb-content)))
+                       :raw-content (append nb-content nil))))
 
 ;;; Managing/listing the content hierarchy
 
@@ -153,7 +143,10 @@ ERRBACK of arity 1 for the contents."
   (let ((content (make-ein:$content
                   :url-or-port url-or-port
                   :notebook-version (ein:notebook-version-numeric url-or-port)
-                  :path path)))
+                  :path path))
+        (raw-content (if (vectorp (plist-get data :content))
+                         (append (plist-get data :content) nil)
+                       (plist-get data :content))))
     (setf (ein:$content-name content) (plist-get data :name)
           (ein:$content-path content) (plist-get data :path)
           (ein:$content-type content) (plist-get data :type)
@@ -162,7 +155,7 @@ ERRBACK of arity 1 for the contents."
           (ein:$content-format content) (plist-get data :format)
           (ein:$content-writable content) (plist-get data :writable)
           (ein:$content-mimetype content) (plist-get data :mimetype)
-          (ein:$content-raw-content content) (plist-get data :content))
+          (ein:$content-raw-content content) raw-content)
     content))
 
 (defun ein:content-query-hierarchy* (url-or-port path callback sessions depth content)
@@ -254,42 +247,20 @@ ERRBACK of arity 1 for the contents."
   (when errcb
     (apply errcb errcbargs)))
 
-(defun ein:content-legacy-rename (content new-path callback cbargs)
-  (let ((path (substring new-path 0 (or (cl-position ?/ new-path :from-end t) 0)))
-        (name (substring new-path (or (cl-position ?/ new-path :from-end t) 0))))
-    (ein:query-singleton-ajax
-     (ein:content-url content)
-     :type "PATCH"
-     :data (json-encode `((name . ,name)
-                          (path . ,path)))
-     :parser #'ein:json-read
-     :success (apply-partially #'update-content-path-legacy content callback cbargs)
-     :error (apply-partially #'ein:content-rename-error new-path))))
-
-(cl-defun update-content-path-legacy (content callback cbargs &key data &allow-other-keys)
-  (setf (ein:$content-path content) (ein:trim-left (format "%s/%s" (plist-get data :path) (plist-get data :name))
-                                                   "/")
-        (ein:$content-name content) (plist-get data :name)
-        (ein:$content-last-modified content) (plist-get data :last_modified))
-  (when callback
-    (apply callback cbargs)))
-
 (defun ein:content-rename (content new-path &optional callback cbargs)
-  (if (>= (ein:$content-notebook-version content) 3)
-      (ein:query-singleton-ajax
-       (ein:content-url content)
-       :type "PATCH"
-       :data (json-encode `((path . ,new-path)))
-       :parser #'ein:json-read
-       :success (apply-partially #'update-content-path content callback cbargs)
-       :error (apply-partially #'ein:content-rename-error (ein:$content-path content)))
-    (ein:content-legacy-rename content new-path callback cbargs)))
+  (ein:query-singleton-ajax
+   (ein:content-url content)
+   :type "PATCH"
+   :data (ein:json-encode `((path . ,new-path)))
+   :parser #'ein:json-read
+   :success (apply-partially #'update-content-path content callback cbargs)
+   :error (apply-partially #'ein:content-rename-error (ein:$content-path content))))
 
 (defun ein:session-rename (url-or-port session-id new-path)
   (ein:query-singleton-ajax
    (ein:url url-or-port "api/sessions" session-id)
    :type "PATCH"
-   :data (json-encode `((path . ,new-path)))
+   :data (ein:json-encode `((path . ,new-path)))
    :complete #'ein:session-rename--complete))
 
 (cl-defun ein:session-rename--complete (&key data response _symbol-status
@@ -337,7 +308,7 @@ Call ERRBACK of arity 1 (contents) upon failure."
                              (format "%s/%s" (plist-get nb-json :path) (plist-get nb-json :name)))
                          (plist-get nb-json :path))))
     (let ((session-hash (make-hash-table :test 'equal)))
-      (dolist (s data (funcall callback session-hash))
+      (dolist (s (append data nil) (funcall callback session-hash))
         (setf (gethash (read-name (plist-get s :notebook)) session-hash)
               (cons (plist-get s :id) (plist-get s :kernel)))))))
 

lisp/ein-core.el
@@ -330,12 +330,11 @@ but can operate in different contexts."
                         ein:get-cell-at-point--shared-output)))
 
 (defun ein:get-traceback-data ()
-  (ein:generic-getter '(ein:get-traceback-data--worksheet
-                        ein:get-traceback-data--shared-output
-                        ein:get-traceback-data--connect)))
+  (append (ein:generic-getter '(ein:get-traceback-data--worksheet
+                                ein:get-traceback-data--shared-output
+                                ein:get-traceback-data--connect))
+          nil))
 
-
-
 ;;; Emacs utilities
 
 (defun ein:clean-compiled-files ()

lisp/ein-dev.el
@@ -319,7 +319,7 @@ Use this function in addition to `pp' (see `ein:dev--pp-to-string')."
         (barf-if-buffer-read-only)
         (erase-buffer)
         (save-excursion
-          (insert (json-encode content-data))
+          (ein:json-insert content-data)
           (json-pretty-print (point-min) (point-max))))
       (pop-to-buffer bufname))))
 

lisp/ein-file.el
@@ -55,10 +55,9 @@
     (pop-to-buffer (buffer-name))))
 
 (defun ein:content-file-save ()
-  (when (boundp 'ein:content-file-buffer--content)
-    (setf (ein:$content-raw-content ein:content-file-buffer--content) (buffer-string))
-    (ein:content-save ein:content-file-buffer--content)
-    (set-buffer-modified-p nil)
-    t))
+  (setf (ein:$content-raw-content ein:content-file-buffer--content) (buffer-string))
+  (ein:content-save ein:content-file-buffer--content)
+  (set-buffer-modified-p nil)
+  t)
 
 (provide 'ein-file)

commit d33e04da06421813cdffed6af18e5379f7399c07

pager fix
This presumably broke after 02ab4522 native json parsing.

Added a test.

Fixed #798

features/notebook.feature
@@ -36,6 +36,15 @@ Scenario: switch kernel
   And I wait for cell to execute
   And I switch kernel to "ir"
 
+@pager
+Scenario: so-called paging in ein:kernel--handle-payload
+  Given new python notebook
+  And I type "help?"
+  And I wait for cell to execute
+  And I switch to buffer like "*ein:pager"
+  And I should see "help()"
+  And I press "C-x k"
+
 @traceback
 Scenario: native json parsing broke traceback
   Given new python notebook

lisp/ein-kernel.el
@@ -517,23 +517,23 @@ Example::
 
 (defun ein:kernel--handle-payload (kernel callbacks payload)
   (cl-loop with events = (ein:$kernel-events kernel)
-        for p in payload
-        for text = (or (plist-get p :text) (plist-get (plist-get p :data) :text/plain))
-        for source = (plist-get p :source)
-        if (member source '("IPython.kernel.zmq.page.page"
-                            "IPython.zmq.page.page"
-                            "page"))
-        do (when (not (equal (ein:trim text) ""))
-             (ein:events-trigger
-              events 'open_with_text.Pager (list :text text)))
-        else if
-        (member
-         source
-         '("IPython.kernel.zmq.zmqshell.ZMQInteractiveShell.set_next_input"
-           "IPython.zmq.zmqshell.ZMQInteractiveShell.set_next_input"
-           "set_next_input"))
-        do (let ((cb (plist-get callbacks :set_next_input)))
-             (when cb (ein:funcall-packed cb text)))))
+           for p in (append payload nil)
+           for text = (or (plist-get p :text) (plist-get (plist-get p :data) :text/plain))
+           for source = (plist-get p :source)
+           if (member source '("IPython.kernel.zmq.page.page"
+                               "IPython.zmq.page.page"
+                               "page"))
+           do (unless (equal (ein:trim text) "")
+		(ein:events-trigger
+		 events 'open_with_text.Pager (list :text text)))
+           else if
+           (member
+            source
+            '("IPython.kernel.zmq.zmqshell.ZMQInteractiveShell.set_next_input"
+              "IPython.zmq.zmqshell.ZMQInteractiveShell.set_next_input"
+              "set_next_input"))
+           do (let ((cb (plist-get callbacks :set_next_input)))
+		(when cb (ein:funcall-packed cb text)))))
 
 (defun ein:kernel--handle-shell-reply (kernel packet)
   (cl-destructuring-bind