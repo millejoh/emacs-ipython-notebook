6f76f0dcbe15eadfa992699d7f3f887b17743892
dickmao <none>
2021-08-09 14:00:39 -0400

    ein:run masked by autoload

--- lisp/ob-ein.el
+++ lisp/ob-ein.el
@@ -55,7 +55,7 @@
 (autoload 'ein:process-url-match "ein-process")
 (autoload 'ein:process-refresh-processes "ein-process")
 (autoload 'ein:jupyter-server-conn-info "ein-jupyter")
-(autoload 'ein:jupyter-server-start "ein-jupyter")
+(autoload 'ein:jupyter-server-start "ein-jupyter" nil t)
 (autoload 'ein:shared-output-get-cell "ein-shared-output")
 (autoload 'ein:shared-output-eval-string "ein-shared-output")
 (autoload 'ein:kernel-live-p "ein-kernel")
